9ba9b0d43948c61487b6ee2160c8dd2f5c96d11f
fardragon <michaldrozd@protonmail.ch>
2024-12-01 14:35:27 +0100

    Add zls checks

--- test/ziglang/test.sh
+++ test/ziglang/test.sh
@@ -4,7 +4,13 @@ source test-utils.sh
 
 # Template specific tests
 check "distro" lsb_release -c
-check "zig" [ "$(zig version)" == "0.12.0" ]
+
+check "zig_version" zig version
+check "zig_version_compare" [ "$(zig version)" == "0.13.0" ]
+
+check "zls_version" zls --version
+check "zls_version_compare" [ "$(zls --version)" == "0.13.0" ]
+
 
 # Report result
 reportResults
