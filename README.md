be54ce1b8348e451185ebb10779c31d65f97d07d
Michał Drozd <michaldrozd@protonmail.ch>
2025-06-19 14:12:27 +0200

    Add zig 0.14.1 (#6)
    * Add zig 0.14.1
    * Handle new tarball naming scheme

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ This repo provides a devcontainer template for Ziglang development.
 ## Supported Zig versions
 * 0.13.0
 * 0.14.0
+* 0.14.1
 
 
 ## Included tools
--- test/ziglang/test.sh
+++ test/ziglang/test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-cd $(dirname "$0")
+cd "$(dirname "$0")" || exit 1
 source test-utils.sh
 
 # Template specific tests
 check "distro" lsb_release -c
 
 check "zig_version" zig version
-check "zig_version_compare" [ "$(zig version)" == "0.14.0" ]
+check "zig_version_compare" [ "$(zig version)" == "0.14.1" ]
 
 check "zls_version" zls --version
 check "zls_version_compare" [ "$(zls --version)" == "0.14.0" ]
