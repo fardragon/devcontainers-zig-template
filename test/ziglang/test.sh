c4d5978e0de560df358469423301895ddb423196
fardragon <michaldrozd@protonmail.ch>
2025-06-19 14:10:50 +0200

    Fix test

--- test/ziglang/test.sh
+++ test/ziglang/test.sh
@@ -6,7 +6,7 @@ source test-utils.sh
 check "distro" lsb_release -c
 
 check "zig_version" zig version
-check "zig_version_compare" [ "$(zig version)" == "0.14.0" ]
+check "zig_version_compare" [ "$(zig version)" == "0.14.1" ]
 
 check "zls_version" zls --version
 check "zls_version_compare" [ "$(zls --version)" == "0.14.0" ]
