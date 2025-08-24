132b8671b0515689c45015eb3103e5c0fd8d2090
fardragon <michaldrozd@protonmail.ch>
2025-08-24 21:01:34 +0200

    Bump zig to 0.15.1

--- test/ziglang/test.sh
+++ test/ziglang/test.sh
@@ -6,10 +6,10 @@ source test-utils.sh
 check "distro" lsb_release -c
 
 check "zig_version" zig version
-check "zig_version_compare" [ "$(zig version)" == "0.14.1" ]
+check "zig_version_compare" [ "$(zig version)" == "0.15.1" ]
 
 check "zls_version" zls --version
-check "zls_version_compare" [ "$(zls --version)" == "0.14.0" ]
+check "zls_version_compare" [ "$(zls --version)" == "0.15.0" ]
 
 
 # Report result
