88b05f85f19dba13da269fa9dcd8c9716fef8afd
fardragon <michaldrozd@protonmail.ch>
2026-04-16 22:20:54 +0200

    Add Zig 0.16.0

--- test/ziglang/test.sh
+++ test/ziglang/test.sh
@@ -6,10 +6,10 @@ source test-utils.sh
 check "distro" lsb_release -c
 
 check "zig_version" zig version
-check "zig_version_compare" [ "$(zig version)" == "0.15.2" ]
+check "zig_version_compare" [ "$(zig version)" == "0.16.0" ]
 
 check "zls_version" zls --version
-check "zls_version_compare" [ "$(zls --version)" == "0.15.0" ]
+check "zls_version_compare" [ "$(zls --version)" == "0.16.0" ]
 
 
 # Report result
